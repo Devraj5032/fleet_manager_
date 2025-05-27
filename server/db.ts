c456104ee6b0e0b7908b8c9f0a356d64739a2fec
Devraj Srivastawa <100594605+Devraj5032@users.noreply.github.com>
2025-05-28 01:19:48 +0530

    push

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,11 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT || 3306),
+  host: 'rovers.cjc26ma2u8ql.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'rovers',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
